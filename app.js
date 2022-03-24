e8255aa976c50b3665a7bff66655fe5f83e67462
Vivy Vuong <vngvy48@gmail.com>
2022-03-24 13:43:47 -0400

    Fixing heroku bugs

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 "use strict";
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
