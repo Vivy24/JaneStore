738e79b0625d5866961544cb08a0ef49a4e39282
Vivy Vuong <vngvy48@gmail.com>
2022-03-23 20:10:13 -0400

    Fixing bug when placing order

--- public/script.js
+++ public/script.js
@@ -52,76 +52,14 @@ getData().then(() => {
             orderData,
             JSON.stringify(orderData, null, 2)
           );
-          var transaction = orderData.purchase_units[0].payments.captures[0];
-          alert(
-            "Transaction " +
-              transaction.status +
-              ": " +
-              transaction.id +
-              "\n\nSee console for all available details"
-          );
 
-          // When ready to go live, remove the alert and show a success message within this page. For example:
-          // var element = document.getElementById('paypal-button-container');
-          // element.innerHTML = '';
-          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-          // Or go to another URL:  actions.redirect('thank_you.html');
-        });
+          // detail of the transaction;
+          var transaction = orderData.purchase_units[0].payments.captures[0];
+
+          const form = document.querySelector(".checkoutForm");
+          form.submit();
+        
       },
     })
     .render("#paypal-button-container");
 });
-
-//  paypal.Buttons({
-//      createOrder: function(data,actions){
-//          return actions.order.create({
-//            purchase_units: items
-//          })},
-
-//          onApprove: function(data, actions){
-//            return actions.order.capture().then(function(orderData){
-// console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-//            })
-//            var transaction = orderData.purchase_units[0].payments.captures[0];
-//                 alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
-
-//             // When ready to go live, remove the alert and show a success message within this page. For example:
-//             // var element = document.getElementById('paypal-button-container');
-//             // element.innerHTML = '';
-//             // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-//             // Or go to another URL:  actions.redirect('thank_you.html');
-//           });
-//         }
-//       }).render('#paypal-button-container');
-
-//paypal
-//   .Buttons({
-//     createOrder: function () {
-//       return fetch("/create-order", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           items: items,
-//         }),
-//       })
-//         .then((res) => {
-//           console.log("This is res", res);
-//           if (res.ok) return res.json();
-//           return res.json().then((json) => Promise.reject(json));
-//         })
-//         .then(({ id }) => {
-//           console.log({ id });
-//           return id;
-//         })
-//         .catch((e) => {
-//           console.error(e.error);
-//         });
-//     },
-//     onApprove: function (data, actions) {
-//       console.log({ data });
-//       return actions.order.capture();
-//     },
-//   })
-//   .render("#paypal");
